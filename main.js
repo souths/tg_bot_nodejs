279a1cf2297f50741c6a372dd2a7a236e10da87c
2272230377 <2272230377@qq.com>
2021-09-05 12:52:11 +0800

    message

--- main.js
+++ main.js
@@ -24,27 +24,29 @@ Date.prototype.Format = function (fmt) {
 };
 
 function main(params) {
-  if(tg_bot_token&&tg_user_id){
+  if (tg_bot_token && tg_user_id) {
     const bot = new Telegraf(tg_bot_token)
     bot.start((ctx) => ctx.reply('Welcome'))
     bot.help((ctx) => ctx.reply('Send me a sticker 机器人bot'))
     bot.on('sticker', (ctx) => ctx.reply('👍'))
     bot.hears('hi', (ctx) => ctx.reply('Hey there'))
     bot.hears('time', (ctx) => ctx.reply(new Date().Format('yyyy-MM-dd HH:mm:ss')))
-    bot.telegram.sendMessage(tg_user_id,'tg bot机器人 nodejs版启用成功')
+    bot.telegram.sendMessage(tg_user_id, 'tg bot机器人 nodejs版启用成功')
     bot.on('text', (ctx) => {
       // Explicit usage
-      ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`)
-    
+      ctx.telegram.sendMessage(ctx.message.chat.id, `text欢迎你输入文字 ${ctx}`)
+
       // Using context shortcut
-      ctx.reply(`欢迎你输入文字 ${ctx.state.role}`)
     })
+    bot.on('message', function (ctx, next) {
+      ctx.telegram.sendMessage(ctx.message.chat.id,`message欢迎你输入文字 ${ctx}`)
+    });
     bot.launch()
-  
+
     // Enable graceful stop
     process.once('SIGINT', () => bot.stop('SIGINT'))
     process.once('SIGTERM', () => bot.stop('SIGTERM'))
-  }else{
+  } else {
     console.log(`tg_bot_token=${tg_bot_token},==,tg_user_id=${tg_user_id}`)
   }
 
